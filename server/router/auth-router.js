ddffb5fbc0a097a36c7cadedb2b36d640a2a6d46
emiloapp <emiloventures@emilo.in>
2025-04-25 13:58:06 +0530

    push controller

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const authController = require('../controller/auth-controller');
 
-router.route("/home").get(authController.home);
-router.route("/getuser").get(authController.register);
-router.route("/register").post(authController.register);
-router.route("/login").post(authController.login);
-router.route("/picwheel").post(authController.picwheel);
-router.route("/picwheelmedia").post(authController.WheelMedia);
 
-module.exports = router;
+router.route("/").get(authController.home);
+router.route("/signup").post(authController.signup);
+router.route("/login").get(authController.login);
+
+
+
+
+module.exports=router;
--- server/utils/db.js
+++ server/utils/db.js
@@ -1,20 +1,16 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
-
-//const URI = "mongodb://localhost:27017/mern_admin";
-//mongoose.connect(URI)
 const URI = process.env.MONGODB_URI;
-
-const connectDB = async()=>{
-    try{
-        await mongoose.connect(URI);
-        console.log("connction success");
-
-    }
-    catch(error){
-        console.error("connction failed");
-        process.exit(0);
-    }
+const connectDB = async ()=>{
+try{
+await mongoose.connect(URI);
+console.log("Connection Successfully Done");
 }
+catch(error){
+console.error("Connection Failed");
+process.exit(0); 
+}
+};
 
 module.exports = connectDB;
