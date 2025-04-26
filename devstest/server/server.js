9cb80f83d8093c38e752f49c8bdbd8c6f74fddd4
omparmarforu <99855160+omparmarforu@users.noreply.github.com>
2025-04-26 13:02:06 +0530

    checking monodb connection status

--- devstest/server/server.js
+++ devstest/server/server.js
@@ -6,9 +6,10 @@ const connectDB = require("./utils/db");
 api.use(express.json());
 api.use("/api/auth/", router);
 
-connectDB();
-
 const PORT = 5000;
+connectDB().then(()=>{
 api.listen(PORT, ()=>{
 console.log(`Server is running on port ${PORT}`);
 });
+})
+.catch((err) => console.log('DB Connection Error: ', err));
