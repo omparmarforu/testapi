1c257715f95ed92a07dc695685ab6ec2b24834c6
omparmarforu <99855160+omparmarforu@users.noreply.github.com>
2025-05-02 11:37:51 +0530

    Update user.js
    Updated

--- devstest/server/models/user.js
+++ devstest/server/models/user.js
@@ -6,9 +6,8 @@ const userSchema = new mongoose.Schema({
 "countrycode" : {type:String, required: true},
 "mobileno" : {type:Number, required: true},
 "password" : {type: String, required: true},
-"age" : {type:Number, required: true},
 "gender" : {type: String, required: true},
-"dob" : {type:Date, required: true},
+"dob" : {type:String, required: true},
 });
 
 const User = new mongoose.model("User",userSchema);
