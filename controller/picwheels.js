11fa2a22d86f915e3f98456f2333fb3cc8b61000
omparmarforu <99855160+omparmarforu@users.noreply.github.com>
2025-05-06 12:05:27 +0530

    Update picwheels.js

--- controller/picwheels.js
+++ controller/picwheels.js
@@ -64,7 +64,7 @@ const createWheel = async (req, res) => {
     try{
       const wheelid = req.query;
       if(!wheelid) return res.status(400).json({ error: "Missing wheelid" });
-      const wheelMedia = await pWheelMedia.findById({wheelid});
+      const wheelMedia = await pWheelMedia.find({wheelid});
       res.status(200).json({
         success: true,    
         data: wheelMedia  
