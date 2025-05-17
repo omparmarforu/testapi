bd273260f9dfae9296fffcd4bd55a2989c849445
omparmarforu <99855160+omparmarforu@users.noreply.github.com>
2025-05-17 18:17:40 +0530

    updated

--- router/auth-router.js
+++ router/auth-router.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 const authController = require('../controller/auth-controller');
 const picWheel  = require('../controller/picwheels');
+const picLike = require('../controller/likemedia');
 const upload = require('../middlewares/uploads');
+const allusers  = require('../controller/users');
 
 
 router.route("/").get(authController.home);
 router.route("/signup").post(authController.signup);
 router.route("/login").post(authController.login);
+router.route("/allusers").get(allusers.getAllUsers);
 
 // Upload wheelcover (1) + media (multiple)
 router.post('/picwheel', upload.fields([
@@ -16,6 +19,7 @@ router.post('/picwheel', upload.fields([
   ]), picWheel.createWheel);
   
 // Get routes
-router.get('/picwheel', picWheel.getWheel);              // All or filtered wheels
+router.get('/picwheel', picWheel.getWheel);              
 router.get('/picwheelmedia', picWheel.getWheelMedia); 
-  module.exports = router;
+router.post('/picwheellike', picLike.addLike); 
+module.exports = router;
