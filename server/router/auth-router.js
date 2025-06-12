735376a6571b9efd20be51869ed348e5ee500b68
omparmarforu <99855160+omparmarforu@users.noreply.github.com>
2025-06-12 15:09:42 +0530

    Updating all files

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const authController = require('../controller/auth-controller');
-const { picWheel } = require('../controller/picwheels');
+const picWheel  = require('../controller/picwheels');
+const picLike = require('../controller/likemedia');
 const upload = require('../middlewares/uploads');
+const allusers  = require('../controller/users');
 
 
-router.route("/").get(authController.home);
-router.route("/signup").post(authController.signup);
+router.route("/home").get(authController.home);
+router.route("/register").post(authController.register);
 router.route("/login").post(authController.login);
+router.route("/search").get(allusers.searchUsers);
 
 // Upload wheelcover (1) + media (multiple)
 router.post('/picwheel', upload.fields([
     { name: 'wheelcover', maxCount: 1 },
     { name: 'media', maxCount: 10 }
-  ]), picWheel);
+  ]), picWheel.createWheel);
   
-  module.exports = router;
+// Get routes
+router.get('/picwheel', picWheel.getWheel);              
+router.get('/picwheelmedia', picWheel.getWheelMedia); 
+router.post('/picwheellike', picLike.addLike); 
+module.exports = router;
